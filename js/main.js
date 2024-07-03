059b31f6b3794cb499c30a7a194bdf1d1620ed9d
Kiamova Elvira <ela.kiyamova@yandex.ru>
2024-07-03 18:11:00 +0500

    fix: correct the names of variables and functions

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 
-const arrMessages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const listMessages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
-const arrNames = ['Тимофей', 'Вера', 'Родион', 'Даниил', 'Михаил', 'Илья', 'Аделина', 'Арсений', 'Александра', 'Иван', 'Дарья', 'Роман', 'Александр', 'Ксения', 'Марк', 'Никита', 'Екатерина', 'София', 'Глеб', 'Кира'];
+const listNames = ['Тимофей', 'Вера', 'Родион', 'Даниил', 'Михаил', 'Илья', 'Аделина', 'Арсений', 'Александра', 'Иван', 'Дарья', 'Роман', 'Александр', 'Ксения', 'Марк', 'Никита', 'Екатерина', 'София', 'Глеб', 'Кира'];
 
-const arrDescriptionPhoto = ['Пляж у озера', 'Указатель', 'Вид на песчаный пляж', 'Девушка в купальнике', 'Рагу с веселым рисом', 'Черная машина', 'Клубника на тарелке', 'Клюквенный морс', 'Самолет над пляжем', 'Обувница', 'Тропинка к пляжу', 'Белая машина', 'Рыба с овощами', 'Кото-ролл', 'Космические домашние тапочки', 'Вид на горы из иллюминатора', 'Хор', 'Красная машина', 'Тапочки с подсветкой', 'Площадь с пальмами', 'Салат', 'Пляж на закате', 'Краб', 'Концерт', 'Переправа'];
+const listPhotoDescriptions = ['Пляж у озера', 'Указатель', 'Вид на песчаный пляж', 'Девушка в купальнике', 'Рагу с веселым рисом', 'Черная машина', 'Клубника на тарелке', 'Клюквенный морс', 'Самолет над пляжем', 'Обувница', 'Тропинка к пляжу', 'Белая машина', 'Рыба с овощами', 'Кото-ролл', 'Космические домашние тапочки', 'Вид на горы из иллюминатора', 'Хор', 'Красная машина', 'Тапочки с подсветкой', 'Площадь с пальмами', 'Салат', 'Пляж на закате', 'Краб', 'Концерт', 'Переправа'];
 
 /**
  * Функция по получению случайного числа из диапазона
@@ -23,17 +23,22 @@ const getRandomInteger = (a, b) => {
  * @param {object} elements - массив данных
  * @returns {string} element - элемент массива
  */
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+const searchRandomElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-function creatingListMessages() {
+/**
+ * Функция создания списка сообщений к фотографии
+ * @param {object} - массив данных
+ * @returns {object} commentsUser - массив объеков в виде комментариев к фото
+ */
+function creatCommentsPhoto() {
   return Array.from({length: getRandomInteger(0, 30)}, () => {
-    const commentUser = {
+    const commentsUser = {
       id: +String(Math.random()).replace(/^0+/, '').replace('.', ''),
       avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-      message: getRandomArrayElement(arrMessages),
-      name: getRandomArrayElement(arrNames),
+      message: searchRandomElement(listMessages),
+      name: searchRandomElement(listNames),
     };
-    return commentUser;
+    return commentsUser;
   });
 }
 
@@ -42,22 +47,20 @@ function creatingListMessages() {
  * @param {object} [] - массив данных пользователя
  * @returns {object} [] - массив объектов
  */
-function generateRandomObject(index) {
+function generateRandomPhotos(index) {
   return {
     id: index,
     url: `photos/${index}.jpg`,
-    description: arrDescriptionPhoto[index - 1],
+    description: listPhotoDescriptions[index - 1],
     likes: getRandomInteger(15, 200),
-    comments: creatingListMessages(arrMessages),
+    comments: creatCommentsPhoto(),
   };
 }
 
 /**
  * Создание массива из 25 сгенерированных объектов
  * @param {number} 25 - количество выведенных объектов
- * @param {function} generateRandomObject - функция для генерации случайного объекта
+ * @param {function} generateRandomPhotos - функция для генерации случайного объекта
  * @returns {object} arrayData - массив объектов
  */
-const arrayData = Array.from({length: 25}, (_, index) => generateRandomObject(++index));
-
-console.log(arrayData);
+Array.from({length: 25}, (_, index) => generateRandomPhotos(++index));
