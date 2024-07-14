857890bcb7bc3c5dd374e7d0026e93a3e24b524b
Kiamova Elvira <ela.kiyamova@yandex.ru>
2024-07-14 15:51:42 +0500

    refactor: transfer the rendering of photos to main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-import './miniatures.js';
+import {generatePhotos} from './data.js';
+import {drawingPhotos} from './miniatures.js';
+
+const photosData = generatePhotos(25);
+
+drawingPhotos(photosData);
--- js/miniatures.js
+++ js/miniatures.js
@@ -1,17 +1,19 @@
-import {generatePhotos} from './data.js';
 const templateUserImage = document.querySelector('#picture').content;
 const containerWithPhotos = document.querySelector('.pictures');
 const fragmentWithPhotos = document.createDocumentFragment();
-const arrayDataFromPhotos = generatePhotos(25);
 
-arrayDataFromPhotos.forEach(({url, likes, comments}) => {
-  const photoElement = templateUserImage.cloneNode(true);
+function drawingPhotos(data) {
+  data.forEach(({url, likes, comments}) => {
+    const photoElement = templateUserImage.cloneNode(true);
 
-  photoElement.querySelector('.picture__img').src = url;
-  photoElement.querySelector('.picture__comments').textContent = comments.length;
-  photoElement.querySelector('.picture__likes').textContent = likes;
+    photoElement.querySelector('.picture__img').src = url;
+    photoElement.querySelector('.picture__comments').textContent = comments.length;
+    photoElement.querySelector('.picture__likes').textContent = likes;
 
-  fragmentWithPhotos.appendChild(photoElement);
-});
+    fragmentWithPhotos.appendChild(photoElement);
+  });
 
-containerWithPhotos.appendChild(fragmentWithPhotos);
+  containerWithPhotos.appendChild(fragmentWithPhotos);
+}
+
+export {drawingPhotos};
