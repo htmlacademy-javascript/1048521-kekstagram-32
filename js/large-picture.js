b7a9cc515f3d818844c921a2ce5eb56fe5e8ba3e
Kiamova Elvira <ela.kiyamova@yandex.ru>
2024-07-15 23:16:25 +0500

    feat: data substitution in a large photo

--- js/large-picture.js
+++ js/large-picture.js
@@ -1,15 +1,29 @@
 const containerPictures = document.querySelector('.pictures');
 const bigPicture = document.querySelector('.big-picture');
-const buttonBigPictureCancel = document.querySelector('.big-picture__cancel');
+const buttonBigPictureCancel = bigPicture.querySelector('.big-picture__cancel');
+const srcBigPhoto = bigPicture.querySelector('.big-picture__img').querySelector('img');
+const socialCaption = bigPicture.querySelector('.social__caption');
+// const likesCount = bigPicture.querySelector('.likes-count');
+// const socialCommentShownCount = bigPicture.querySelector('.social__comment-shown-count');
 
 function renderFullSizeImage (evt) {
   if (evt.target.closest('.picture__img')) {
+    console.log(evt.target);
+    srcBigPhoto.src = evt.target.src;
+    socialCaption.textContent = evt.target.alt;
+    // likesCount.textContent = evt.target.likes;
     bigPicture.classList.remove('hidden');
+    document.querySelector('body').classList.add('modal-open');
+
+    document.querySelector('.social__comment-count').classList.add('hidden');
+    document.querySelector('.comments-loader').classList.add('hidden');
   }
 }
 containerPictures.addEventListener('click', renderFullSizeImage);
 
 function closeLargePhoto () {
   bigPicture.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
 }
 buttonBigPictureCancel.addEventListener('click', closeLargePhoto);
+
--- js/miniatures.js
+++ js/miniatures.js
@@ -3,12 +3,13 @@ const containerWithPhotos = document.querySelector('.pictures');
 const fragmentWithPhotos = document.createDocumentFragment();
 
 function drawingPhotos(data) {
-  data.forEach(({url, likes, comments}) => {
+  data.forEach(({url, likes, comments, description}) => {
     const photoElement = templateUserImage.cloneNode(true);
 
     photoElement.querySelector('.picture__img').src = url;
     photoElement.querySelector('.picture__comments').textContent = comments.length;
     photoElement.querySelector('.picture__likes').textContent = likes;
+    photoElement.querySelector('.picture__img').alt = description;
 
     fragmentWithPhotos.appendChild(photoElement);
   });
