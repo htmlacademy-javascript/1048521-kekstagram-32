6429ab3a49774b02fe68baa4eebc7dc3c7ba302a
Kiamova Elvira <ela.kiyamova@yandex.ru>
2024-08-03 11:29:30 +0500

    feat: add adjustment to the number of displayed photos

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 import {drawingPhotos} from './miniatures.js';
 import {addHandlersToForm} from './form.js';
+const COUNT_RENDERED_PHOTOS = 25;
 
 addHandlersToForm();
 
 fetch('https://32.javascript.htmlacademy.pro/kekstagram/data')
   .then((response) => response.json())
   .then((photos) => {
-    drawingPhotos(photos);
+    console.log(photos);
+    drawingPhotos(photos.slice(0, COUNT_RENDERED_PHOTOS));
   });
