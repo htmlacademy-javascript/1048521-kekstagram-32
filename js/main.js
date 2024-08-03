1709c30f23a5ac39041391aec821c3a4291681b2
Kiamova Elvira <ela.kiyamova@yandex.ru>
2024-08-03 11:32:46 +0500

    fix: remove the console

--- js/main.js
+++ js/main.js
@@ -7,6 +7,5 @@ addHandlersToForm();
 fetch('https://32.javascript.htmlacademy.pro/kekstagram/data')
   .then((response) => response.json())
   .then((photos) => {
-    console.log(photos);
     drawingPhotos(photos.slice(0, COUNT_RENDERED_PHOTOS));
   });
